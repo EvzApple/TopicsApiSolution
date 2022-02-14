33667316f0ec5fddb4025794b4eed11cf3e03bca
EvzApple <emondra1@progressive.com>
2022-02-14 12:34:24 -0500

    Add project files.

---
+++ TopicsApi/Controllers/StatusController.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TopicsApi.Controllers;
+
+public class StatusController :ControllerBase
+{
+    [HttpGet("status/oncalldeveloper")] 
+    public ActionResult GetOnCallDeveloper()
+    {
+        return Ok();
+    }
+}
